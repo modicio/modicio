cbbc263ad23ba082db0c786394acd2adfec716c4
Karl Kegel <karl.kegel@mailbox.tu-dresden.de>
2022-10-10 08:52:34 +0200

    Fix tests for PR 23

--- src/test/scala/modicio/codi/rules/ParentRelationRuleSpec.scala
+++ src/test/scala/modicio/codi/rules/ParentRelationRuleSpec.scala
@@ -1,15 +1,16 @@
 package modicio.codi.rules
 
-import modicio.codi.Rule
+import modicio.core.Rule
+import modicio.core.rules.ParentRelationRule
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
 
-class ExtensionRuleSpec extends AnyFlatSpec with should.Matchers {
+class ParentRelationRuleSpec extends AnyFlatSpec with should.Matchers {
 
   val nativeStringNoId = ":#:Project"
 
   "An ExtensionRule" must "be correctly constructed from a (native value) DSL string with empty id" in {
-    val extensionRule = new ExtensionRule(nativeStringNoId)
+    val extensionRule = new ParentRelationRule(nativeStringNoId)
     extensionRule.parentName should equal ("Project")
     extensionRule.parentIdentity should equal ("#")
     extensionRule.id should be (Rule.UNKNOWN_ID)
