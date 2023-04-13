bc9436c1ef59f174288556de473d255b0988327e
Minji Kim <59950900+minjikim23@users.noreply.github.com>
2023-04-13 11:25:10 +0200

    fix error
    Signed-off-by: Minji Kim <minji.kim@3m5.de>

--- src/test/scala/modicio/MonitoringRegistryFixture.scala
+++ src/test/scala/modicio/MonitoringRegistryFixture.scala
@@ -1,8 +1,10 @@
 package modicio
 
 import modicio.core.monitoring.Monitoring
+import modicio.SimpleMapRegistryFixture
 class MonitoringRegistryFixture extends RegistryFixture {
-	val registry = new Monitoring(new SimpleMapRegistryFixture().registry, typeFactory, instanceFactory)
+	val simpleMapRegistryFixture = new SimpleMapRegistryFixture
+	val registry = new Monitoring(simpleMapRegistryFixture.registry, typeFactory, instanceFactory)
 	typeFactory.setRegistry(registry)
 	instanceFactory.setRegistry(registry)
 }
