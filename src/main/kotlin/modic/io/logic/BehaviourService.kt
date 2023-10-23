76bcf7588698a7d0c580d00d366f9ae0cb1ed118
Karl Kegel <private@kkegel.de>
2023-10-23 11:45:23 +0200

    Prepare behaviour endpoints

---
+++ src/main/kotlin/modic/io/logic/BehaviourService.kt
@@ -0,0 +1,44 @@
+/**
+ * Copyright 2023 Karl Kegel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package modic.io.logic
+
+import jakarta.transaction.Transactional
+import modic.io.model.Fragment
+import modic.io.repository.FragmentRepository
+import org.springframework.stereotype.Service
+
+@Service
+class BehaviourService(
+    val fragmentRepository: FragmentRepository
+) {
+
+    @Transactional
+    fun triggerStoredActionScript(fragmentDataID: Long, scriptURI: String): Fragment? {
+        val targetFragment = fragmentRepository.getFragmentByDataID(fragmentDataID)
+        if(targetFragment != null){
+            val script = targetFragment.model!!.findScript(scriptURI) ?: throw Exception("No executable script found")
+
+            /*
+            TODO use the script to edit the fragment
+             */
+
+            return targetFragment
+        }
+        return null
+    }
+
+}
