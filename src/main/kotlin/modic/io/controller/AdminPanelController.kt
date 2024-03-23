75e25b447d6754576b04d5a13396dd484bb39e87
WilliamThimm <William.Thimm@gmail.com>
2024-03-23 16:15:56 +0100

    Started Working on Fragment Detail Page, URL Parameter now works

--- src/main/kotlin/modic/io/controller/AdminPanelController.kt
+++ src/main/kotlin/modic/io/controller/AdminPanelController.kt
@@ -2,33 +2,43 @@ package modic.io.controller
 
 import modic.io.logic.ModelService
 import modic.io.model.Fragment
-import modic.io.repository.TestDataHelper
+import modic.io.repository.FragmentRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
 class AdminPanelController {
+
+    @Autowired
+    lateinit var fragmentRepository: FragmentRepository
 
     @Autowired
     lateinit var modelService : ModelService
 
     @GetMapping("/admin")
     fun getLandingPage(model : Model): String {
-        val referenceFragment = modelService.getReferenceFragment() ?: TestDataHelper.getSimpleFragmentOnlyModel()
+        val referenceFragment = modelService.getReferenceFragment()
         Fragment.renderFragmentToPlantUML(referenceFragment)
         model.addAttribute("diagram", "diagram.jpeg")
-        model.addAttribute("variantName", referenceFragment.variantName)
-        model.addAttribute("variantID", referenceFragment.variantID)
-        model.addAttribute("runningTime", referenceFragment.runningTime)
-        model.addAttribute("runningID", referenceFragment.runningID)
+        model.addAttribute("variantName", referenceFragment?.variantName)
+        model.addAttribute("variantID", referenceFragment?.variantID)
+        model.addAttribute("runningTime", referenceFragment?.runningTime)
+        model.addAttribute("runningID", referenceFragment?.runningID)
         return "referenceFragment"
     }
 
     @GetMapping("/admin/variants")
     fun displayVariantsAndVersions(model: Model): String {
         return "variants"
+    }
+
+    @GetMapping("/admin/fragmentDetails")
+    fun displayFragmentDetails(model: Model, @RequestParam fragmentID : String): String{
+        val fragment = fragmentRepository.getFragmentByDataID(fragmentID.toLong())
+        return "fragmentDetails"
     }
 
 
